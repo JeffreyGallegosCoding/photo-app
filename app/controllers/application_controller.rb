ab79c739c671b82c2c0482a419c2ca330ca80b8d
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-11-09 14:18:59 -0700

    Added devise and styling files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # In order to access the application you need to be a logged in user
+  before_action :authenticate_user!
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  # skip the before action on my application controller only for the index
+  # do not have to be signed in to view the homepage
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
 
