332cbd7f319f3c9a9ec836ac448f75d830a5d74b
JeffreyGallegosCoding <jeffreygallegos01@gmail.com>
2020-11-11 16:46:26 -0700

    Created base stripe, code in user, and payment models to handle payment requests upon premium plan form submission

---
+++ app/models/payment.rb
@@ -0,0 +1,32 @@
+class Payment < ApplicationRecord
+  # needed fields for card payments
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
+  belongs_to :user
+
+  # displays for month and year options
+
+  # when a user selects the month option for cc info - display purposes
+  def self.month_options
+    Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]  }
+  end
+
+  def self.year_options
+    # date range for year - Today's year, .. is range, to how many years in the future to display
+    # and convert it into an array
+    (Date.today.year..(Date.today.year + 10)).to_a
+  end
+
+  # method used to process the payment during registration
+  def process_payment
+    # create a stripe customer variable to assign a stripe charge to
+    customer = Stripe::Customer.create email: email, card: token
+
+    # creates a stripe charge assigned to the customer variable by id with the payment attributes
+    # amount 1000 is cents, description of charge, and the type of currency used for charge
+    Stripe::Charge.create customer: customer.id,
+                          amount: 1000,
+                          description: 'Premium',
+                          currency: 'usd'
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  # user will only be required once to sign up for the site
+  has_one :payment
+  # user accepts the payment attributes as well upon form submission
+  # will hit the payments table at the same time the users table is hit
+  accepts_nested_attributes_for :payment
 end
